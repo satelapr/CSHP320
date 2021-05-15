1e4ef5c5697eeae20ad042bc5196ae83cf8223b7
satelapr <77853241+satelapr@users.noreply.github.com>
2021-05-14 22:07:02 -0700

    Add files via upload

---
+++ Assignment03/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var users = new List<Models.User>();
+            users.Add(new Models.User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
+
+            uxList.ItemsSource = users;
+            uxList.AddHandler(GridViewColumnHeader.ClickEvent, new RoutedEventHandler(uxList_OnColumnClick));
+        }
+
+        private void uxList_OnColumnClick(object sender, RoutedEventArgs e)
+        {
+            var headerClicked = e.OriginalSource as GridViewColumnHeader;
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+            if (headerClicked != null)
+            {
+                var columnBinding = headerClicked.Column.DisplayMemberBinding as Binding;
+                var sortBy = columnBinding?.Path.Path ?? headerClicked.Column.Header as string;
+                view.SortDescriptions.Clear();
+                
+                if (sortBy.Equals("Name"))
+                {
+                    view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+                }
+                else
+                {
+                    view.SortDescriptions.Add(new SortDescription("Password", ListSortDirection.Ascending));
+                }
+                view.Refresh();
+            }
+        }
+    }
+}
